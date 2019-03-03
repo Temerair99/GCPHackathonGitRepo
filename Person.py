13283181186c4d55c13e1e4ddebca7f9650c49e8
TylerOviatt <48174633+TylerOviatt@users.noreply.github.com>
2019-03-03 07:38:14 -0800

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,5 +1,7 @@
 #Tyler_Oviatt
-class Person:
+
+
+class Person:#My class for the Name/Rate/Phone
 #Constructor
     def __init__(self, name, rating, phone):
         self.name       = name
@@ -10,8 +12,16 @@ class Person:
     def set_name(self, name):
         self.name       = name
 
+
     def set_rating(self, rating):
-        self.rating    = rating
+       self.rating    = rating
+
+    def up_rate(self):
+        self.rating += 1
+
+    def down_rate(self):
+        self.rating -= 1
+
 
     def set_phone(self, phone):
         self.phone     = phone
@@ -26,3 +36,31 @@ class Person:
 
     def get_phone(self):
         return self.phone
+
+#Test funcion 
+#this allows us to test for a single user 
+
+rating = 0
+
+name    = input('What is your name:')
+phone   = input('Enter your phone:')
+print('Name:', name, '', 'Phone:', phone, '' 'User rating:', rating)
+
+rating  = input ('/1 is yes, 0 is no /Do you want to up vote?:')
+
+if rating == 1:
+    rating.up_rate
+elif rating == 0:
+    rating.down_rate
+
+print('Name:', name, '', 'Phone:', phone, '' 'User rating:', rating)
+
+
+##this was a test for the person class
+
+#Steve = Person(name, phone)
+
+#print("Rating: ", Steve.get_rating())
+
+#Steve.up_rate()
+#print("Rating: ", Steve.get_rating())
