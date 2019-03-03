5ef2215756360a23140e1226be7eb7834a75892b
Clifton <Clifton.Raw@gmail.com>
2019-03-03 01:52:41 -0800

    Added Post File

---
+++ Post.py
@@ -0,0 +1,21 @@
+import numpy as np
+import "Person.py"
+
+class Post:
+
+    def __init__ (self, name, rating, id):
+        self.name   = Person.name
+        self.rating = Person.rating
+        self.id     = id
+
+    def get_user(self, name, rating):
+        return "Name: " + self.name + " Rating: " + self.rating
+
+    def id_name(self, object):
+
+
+    def location():
+        temp2 = 0
+
+    def is_avlb(self):
+        temp1 = 0
