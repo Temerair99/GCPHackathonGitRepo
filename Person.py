21ec973130a35f234f0c29542b8c800a3d863e6a
TylerOviatt <48174633+TylerOviatt@users.noreply.github.com>
2019-03-03 04:08:37 -0800

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,19 +1,20 @@
+#Tyler_Oviatt
 class Person:
 #Constructor
     def __init__(self, name, rating, phone):
-        self.name = name
-        self.rating = rating
-        self.phone = phone
+        self.name       = name
+        self.rating     = rating
+        self.phone      = phone
 
 #Mutator
     def set_name(self, name):
-        self.name = name
+        self.name       = name
 
     def set_rating(self, rating):
-        self.rating = rating
+        self.rating    = rating
 
     def set_phone(self, phone):
-        self.phone = phone
+        self.phone     = phone
 
 #Accessor
 
@@ -25,4 +26,3 @@ class Person:
 
     def get_phone(self):
         return self.phone
-
