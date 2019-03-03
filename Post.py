a6bb08e7af8b3b12ca95c07d2e00ce738fefaea8
Clifton <Clifton.Raw@gmail.com>
2019-03-03 06:38:57 -0800

    Update Post

--- Post.py
+++ Post.py
@@ -1,21 +1,26 @@
-import numpy as np
-import "Person.py"
+from Person import Person
 
 class Post:
 
-    def __init__ (self, name, rating, id):
-        self.name   = Person.name
-        self.rating = Person.rating
-        self.id     = id
+    def __init__ (self, name, rating, id, location, is_avlb):
+        self.name       = Person.get_name
+        self.rating     = Person.get_rating
+        self.__id       = 0
+        self.__location = 0
 
-    def get_user(self, name, rating):
-        return "Name: " + self.name + " Rating: " + self.rating
+    def obj_desc(self):
+        item_desc = input("Item Description: ")
+        return item_desc
 
-    def id_name(self, object):
+    def get_user(self):
+        return "Name: " + str(self.name) + "\nRating: " + str(self.rating)
 
-
-    def location():
+    def get_dest():
         temp2 = 0
 
     def is_avlb(self):
-        temp1 = 0
+        if ():
+
+x = Post(Person.get_name, Person.get_rating, 0, 0, True)
+
+x.get_user()
