43951f4f3be572b5013c996cfa0392f037bb8f6f
RkBass <42913004+RkBass@users.noreply.github.com>
2019-03-03 08:24:48 -0800

    Update GoogleMapsWithFlask.py
    Integrated Geoc_Func (translate physical address to latitude and longitude)

--- GoogleMapsWithFlask.py
+++ GoogleMapsWithFlask.py
@@ -4,6 +4,8 @@ from flask_googlemaps import GoogleMaps
 
 from flask_googlemaps import Map
 
+from Geoc_Func import fetch_geocode
+
 myfile = open('person.txt', 'r')
 mytext = myfile.read()
 myfile.close()
@@ -20,6 +22,10 @@ app = Flask(__name__, template_folder=".")
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyCjmcW8uZhOU0DoZ7kWKjx11WrlJsDsAR4"
 
 
+# Fetch geocode from address
+
+user_address = 'CSU San Bernardino'
+user_lat, user_lng = fetch_geocode(user_address)
 
 # Initialize the extension
 
@@ -77,8 +83,19 @@ def mapview():
 
              'infobox': "<b>Hello World from other place</b>"
 
-          }
+          },
+          
+          {
 
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+
+             'lat': user_lat,
+
+             'lng': user_lng,
+
+             'infobox': user_address
+
+          },
         ]
 
     )
